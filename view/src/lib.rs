4613603e80bda17d21901b1444ffb936983e5b8e
hatsusato <2765630+hatsusato@users.noreply.github.com>
2023-01-18 22:11:52 +0900

    Add screen module

--- view/src/lib.rs
+++ view/src/lib.rs
@@ -1,3 +1,5 @@
+mod screen;
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
---
+++ view/src/screen.rs
@@ -0,0 +1,21 @@
+use crossterm::event::{Event, KeyCode};
+
+struct Screen {}
+impl Screen {
+    pub fn getch() -> Option<char> {
+        if let Ok(Event::Key(key)) = crossterm::event::read() {
+            Self::translate(key.code)
+        } else {
+            None
+        }
+    }
+    fn translate(code: KeyCode) -> Option<char> {
+        use KeyCode::*;
+        Some(match code {
+            Char(c) => c,
+            Enter => '\n',
+            Tab => '\t',
+            _ => return None,
+        })
+    }
+}
