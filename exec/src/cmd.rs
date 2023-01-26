52860268fb2e2fb65c47c464f062b10ac0a60531
hatsusato <2765630+hatsusato@users.noreply.github.com>
2023-01-26 21:12:50 +0900

    Reduce 'Command::'

--- exec/src/cmd.rs
+++ exec/src/cmd.rs
@@ -1,129 +1,129 @@
 use crate::exec::ExecCmd;
-use inst::Command;
+use inst::Command::{self, *};
 use state::State;
 
 const BLOCK_SIDE: u8 = 16;
 
 impl ExecCmd {
     pub fn imm(state: &State, digit: u8) -> Command {
-        Command::Imm(state.data, combine(state.data, digit))
+        Imm(state.data, combine(state.data, digit))
     }
     pub fn swap(state: &State) -> Command {
-        Command::Swap((state.data, state.acc), (state.acc, state.data))
+        Swap((state.data, state.acc), (state.acc, state.data))
     }
     pub fn hi(state: &State) -> Command {
-        Command::Hi(state.data, state.acc)
+        Hi(state.data, state.acc)
     }
     pub fn lo(state: &State) -> Command {
-        Command::Lo(state.acc, state.data)
+        Lo(state.acc, state.data)
     }
     pub fn inc(state: &State) -> Command {
         let (next, _) = state.acc.overflowing_add(1);
-        Command::Inc(state.acc, next)
+        Inc(state.acc, next)
     }
     pub fn dec(state: &State) -> Command {
         let (next, _) = state.acc.overflowing_sub(1);
-        Command::Dec(state.acc, next)
+        Dec(state.acc, next)
     }
     pub fn add(state: &State) -> Command {
         let next = (state.acc as u16) + (state.data as u16);
-        Command::Add((state.data, state.acc), split(next))
+        Add((state.data, state.acc), split(next))
     }
     pub fn sub(state: &State) -> Command {
         let (next, _) = (state.acc as u16).overflowing_sub(state.data as u16);
-        Command::Sub((state.data, state.acc), split(next))
+        Sub((state.data, state.acc), split(next))
     }
     pub fn mul(state: &State) -> Command {
         let next = (state.data as u16) * (state.acc as u16);
-        Command::Mul((state.data, state.acc), split(next))
+        Mul((state.data, state.acc), split(next))
     }
     pub fn div(state: &State) -> Command {
         if state.data == 0 {
-            Command::DivErr(state.error)
+            DivErr(state.error)
         } else {
             let next = (state.acc % state.data, state.acc / state.data);
-            Command::Div((state.data, state.acc), next)
+            Div((state.data, state.acc), next)
         }
     }
     pub fn neg(state: &State) -> Command {
-        Command::Neg(state.acc, extend(state.data == 0))
+        Neg(state.acc, extend(state.data == 0))
     }
     pub fn bool(state: &State) -> Command {
-        Command::Bool(state.acc, extend(state.data != 0))
+        Bool(state.acc, extend(state.data != 0))
     }
     pub fn eq(state: &State) -> Command {
-        Command::Eq(state.acc, extend(state.data == state.acc))
+        Eq(state.acc, extend(state.data == state.acc))
     }
     pub fn lt(state: &State) -> Command {
-        Command::Lt(state.acc, extend(state.data < state.acc))
+        Lt(state.acc, extend(state.data < state.acc))
     }
     pub fn gt(state: &State) -> Command {
-        Command::Lt(state.acc, extend(state.data > state.acc))
+        Lt(state.acc, extend(state.data > state.acc))
     }
     pub fn not(state: &State) -> Command {
-        Command::Not(state.acc, !state.data)
+        Not(state.acc, !state.data)
     }
     pub fn and(state: &State) -> Command {
-        Command::And(state.acc, state.data & state.acc)
+        And(state.acc, state.data & state.acc)
     }
     pub fn or(state: &State) -> Command {
-        Command::Or(state.acc, state.data | state.acc)
+        Or(state.acc, state.data | state.acc)
     }
     pub fn xor(state: &State) -> Command {
-        Command::Xor(state.acc, state.data ^ state.acc)
+        Xor(state.acc, state.data ^ state.acc)
     }
     pub fn shl(state: &State) -> Command {
-        Command::Shl(state.acc, state.acc << 1)
+        Shl(state.acc, state.acc << 1)
     }
     pub fn shr(state: &State) -> Command {
-        Command::Shr(state.acc, state.acc >> 1)
+        Shr(state.acc, state.acc >> 1)
     }
     pub fn rotl(state: &State) -> Command {
-        Command::Rotl(state.acc, rot(state.acc, true))
+        Rotl(state.acc, rot(state.acc, true))
     }
     pub fn rotr(state: &State) -> Command {
-        Command::Rotr(state.acc, rot(state.acc, true))
+        Rotr(state.acc, rot(state.acc, true))
     }
     pub fn left(state: &State) -> Command {
-        Command::Left(state.coord, backward(state, 1))
+        Left(state.coord, backward(state, 1))
     }
     pub fn right(state: &State) -> Command {
-        Command::Right(state.coord, forward(state, 1))
+        Right(state.coord, forward(state, 1))
     }
     pub fn down(state: &State) -> Command {
-        Command::Down(state.coord, forward(state, BLOCK_SIDE))
+        Down(state.coord, forward(state, BLOCK_SIDE))
     }
     pub fn up(state: &State) -> Command {
-        Command::Left(state.coord, backward(state, BLOCK_SIDE))
+        Left(state.coord, backward(state, BLOCK_SIDE))
     }
     pub fn pos(state: &State) -> Command {
-        Command::Pos((state.data, state.acc), (state.block, state.coord))
+        Pos((state.data, state.acc), (state.block, state.coord))
     }
     pub fn goto(state: &State) -> Command {
-        Command::Goto(state.coord, state.acc)
+        Goto(state.coord, state.acc)
     }
     pub fn jump(state: &State) -> Command {
-        Command::Jump(state.block, state.data)
+        Jump(state.block, state.data)
     }
     pub fn load(state: &State) -> Command {
-        Command::Load(state.data, state.page()[state.coord])
+        Load(state.data, state.page()[state.coord])
     }
     pub fn store(state: &State) -> Command {
-        Command::Load(state.page()[state.coord], state.data)
+        Load(state.page()[state.coord], state.data)
     }
     pub fn argc(state: &State) -> Command {
         const MAX_LEN: usize = u8::MAX as usize;
         let len = std::env::args().len().min(MAX_LEN) as u8;
         let overflow = MAX_LEN < std::env::args().len();
-        Command::Argc((state.acc, state.error), (len, overflow))
+        Argc((state.acc, state.error), (len, overflow))
     }
     pub fn argv(state: &State) -> Command {
         if let Some(arg) = std::env::args().nth(state.acc as usize) {
             let mut next = state.page().clone();
             next.write(arg.as_bytes().iter());
-            Command::Argv(*state.page(), next)
+            Argv(*state.page(), next)
         } else {
-            Command::NoArg(state.error)
+            NoArg(state.error)
         }
     }
 }
