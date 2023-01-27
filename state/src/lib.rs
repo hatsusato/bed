666d63c8deaa12c35c4db376b7f1b957855bb456
hatsusato <2765630+hatsusato@users.noreply.github.com>
2023-01-28 01:12:00 +0900

    Remove add

--- state/src/lib.rs
+++ state/src/lib.rs
@@ -3,18 +3,3 @@ mod state;
 
 pub use bank::Bank;
 pub use state::State;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
--- view/src/lib.rs
+++ view/src/lib.rs
@@ -3,18 +3,3 @@ mod print;
 mod screen;
 
 pub use editor::Editor;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
