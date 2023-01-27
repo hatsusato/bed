a2d53dfa9eabcab8161135e824e874cf90d31a87
hatsusato <2765630+hatsusato@users.noreply.github.com>
2023-01-28 00:47:13 +0900

    Add bank

--- state/src/lib.rs
+++ state/src/lib.rs
@@ -1,6 +1,9 @@
+mod bank;
 mod state;
 
+pub use bank::Bank;
 use util::{Block, Page};
+
 pub struct State {
     pub acc: u8,
     pub block: u8,
@@ -8,47 +11,6 @@ pub struct State {
     pub data: u8,
     pub error: bool,
     pub memory: Block<Page>,
-}
-#[derive(Clone)]
-pub struct Bank {
-    pub acc: u8,
-    pub block: u8,
-    pub coord: u8,
-    pub data: u8,
-    pub error: bool,
-    pub page: Option<Page>,
-}
-impl Bank {
-    pub fn update_acc(&self, acc: u8) -> Self {
-        let mut bank = self.clone();
-        bank.acc = acc;
-        bank
-    }
-    pub fn update_block(&self, block: u8) -> Self {
-        let mut bank = self.clone();
-        bank.block = block;
-        bank
-    }
-    pub fn update_coord(&self, coord: u8) -> Self {
-        let mut bank = self.clone();
-        bank.coord = coord;
-        bank
-    }
-    pub fn update_data(&self, data: u8) -> Self {
-        let mut bank = self.clone();
-        bank.data = data;
-        bank
-    }
-    pub fn update_error(&self, error: bool) -> Self {
-        let mut bank = self.clone();
-        bank.error = error;
-        bank
-    }
-    pub fn update_page(&self, page: Page) -> Self {
-        let mut bank = self.clone();
-        bank.page = Some(page);
-        bank
-    }
 }
 
 pub fn add(left: usize, right: usize) -> usize {
