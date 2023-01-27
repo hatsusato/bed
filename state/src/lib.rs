f97b877e94e8aedeffc126cccb5b15b686b1feaa
hatsusato <2765630+hatsusato@users.noreply.github.com>
2023-01-28 01:21:14 +0900

    Merge bank

--- state/src/lib.rs
+++ state/src/lib.rs
@@ -1,5 +1,4 @@
-mod bank;
 mod state;
 
-pub use bank::Bank;
+pub use state::Bank;
 pub use state::State;
--- state/src/state.rs
+++ state/src/state.rs
@@ -1,6 +1,15 @@
-use crate::Bank;
 use util::Block;
 use util::Page;
+
+#[derive(Default)]
+pub struct Bank {
+    pub acc: u8,
+    pub block: u8,
+    pub coord: u8,
+    pub data: u8,
+    pub error: bool,
+    pub page: Option<Page>,
+}
 
 #[derive(Default)]
 pub struct State {
