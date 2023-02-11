933d05a9d19b69df64c9f013411d2bcb1bee1cc2
hatsusato <2765630+hatsusato@users.noreply.github.com>
2023-02-11 16:29:13 +0900

    Remove state

--- vm/src/lib.rs
+++ vm/src/lib.rs
@@ -3,11 +3,9 @@ mod ctrl;
 mod inst;
 mod machine;
 mod page;
-mod state;
 
 pub use bank::Bank;
 pub use ctrl::Ctrl;
 pub use inst::Inst;
 pub use machine::Machine;
 pub use page::Page;
-pub use state::State;
