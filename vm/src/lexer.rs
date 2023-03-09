bf734ec2a8945c052c08b221068af50827a6910d
hatsusato <2765630+hatsusato@users.noreply.github.com>
2023-03-09 22:03:06 +0900

    Add Mode::Ignore

--- vm/src/lexer.rs
+++ vm/src/lexer.rs
@@ -3,6 +3,7 @@ use crate::inst::Inst;
 #[derive(Clone, Copy)]
 enum Mode {
     Normal,
+    Ignore,
 }
 impl Default for Mode {
     fn default() -> Self {
@@ -16,8 +17,31 @@ pub struct Lexer {
 }
 impl Lexer {
     pub fn consume(&mut self, input: char) -> Option<Inst> {
-        match self.mode {
-            Mode::Normal => Some(Inst::new(input)),
+        match input {
+            '\n' => self.consume_newline(),
+            '#' => self.consume_hash(),
+            _ => self.consume_other(input),
         }
+    }
+    fn consume_newline(&mut self) -> Option<Inst> {
+        match self.mode {
+            Mode::Normal => return Some(Inst::Nop),
+            Mode::Ignore => self.mode = Mode::Normal,
+        }
+        None
+    }
+    fn consume_hash(&mut self) -> Option<Inst> {
+        match self.mode {
+            Mode::Normal => self.mode = Mode::Ignore,
+            Mode::Ignore => (),
+        }
+        None
+    }
+    fn consume_other(&mut self, input: char) -> Option<Inst> {
+        match self.mode {
+            Mode::Normal => return Some(Inst::new(input)),
+            Mode::Ignore => (),
+        }
+        None
     }
 }
