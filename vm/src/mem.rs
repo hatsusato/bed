27fadbfd669ac65f46360ad5eb20dbbe0aef8f4e
hatsusato <2765630+hatsusato@users.noreply.github.com>
2023-03-12 16:33:10 +0900

    Add Page

--- vm/src/mem.rs
+++ vm/src/mem.rs
@@ -1,14 +1,59 @@
+use crate::Registers;
+use std::io;
 use util::Block;
 
-#[derive(Default, Clone, Copy)]
-pub struct Page {
-    page: Block<u8>,
+pub struct Page<'a> {
+    regs: &'a mut Registers,
+    page: &'a mut Block<u8>,
 }
-impl Page {
-    pub fn get(&self) -> &Block<u8> {
-        &self.page
+impl<'a> Page<'a> {
+    pub fn new(regs: &'a mut Registers, memory: &'a mut Block<Block<u8>>) -> Self {
+        let page = &mut memory[regs.block];
+        Self { regs, page }
     }
-    pub fn get_mut(&mut self) -> &mut Block<u8> {
-        &mut self.page
+    pub fn load(&mut self) {
+        self.regs.data = self.cur()[0];
+    }
+    pub fn store(&mut self) {
+        self.cur_mut()[0] = self.regs.data;
+    }
+    pub fn delete(&mut self) {
+        self.cur_mut()[0] = 0;
+    }
+    pub fn put(&mut self) {
+        use io::Write;
+        let buf = &self.cur()[0..1];
+        let result = io::stdout().write(buf);
+        self.regs.set_error(result.is_err());
+    }
+    pub fn get(&mut self) {
+        use io::Read;
+        let buf = &mut self.cur_mut()[0..1];
+        let result = io::stdin().read(buf);
+        self.regs.set_error(result.is_err());
+    }
+    pub fn save(&mut self) {
+        let buf = &mut [0; 4];
+        self.regs.save(buf);
+        Self::copy(self.cur_mut(), buf);
+    }
+    pub fn restore(&mut self) {
+        let buf = &mut [0; 4];
+        Self::copy(buf, self.cur());
+        self.regs.restore(buf);
+    }
+    pub fn quote(&mut self, input: &str) {
+        Self::copy(self.cur_mut(), input.as_bytes());
+    }
+    fn cur(&mut self) -> &[u8] {
+        let index = usize::from(self.regs.coord);
+        &self.page.iter().as_slice()[index..]
+    }
+    fn cur_mut(&mut self) -> &mut [u8] {
+        let index = usize::from(self.regs.coord);
+        &mut self.page.iter_mut().into_slice()[index..]
+    }
+    fn copy(dst: &mut [u8], src: &[u8]) {
+        dst.iter_mut().zip(src).for_each(|(dst, src)| *dst = *src);
     }
 }
--- vm/src/state.rs
+++ vm/src/state.rs
@@ -1,17 +1,17 @@
 use crate::mem::Page;
 use crate::{Inst, Registers};
 use screen::Screen;
-use std::io;
 use util::{Block, BLOCK_SIDE};
 
 #[derive(Default)]
 pub struct State {
     regs: Registers,
-    memory: Block<Page>,
+    memory: Block<Block<u8>>,
 }
 impl State {
     pub fn issue(&mut self, inst: Inst) {
         let regs = &mut self.regs;
+        let mut page = Page::new(regs, &mut self.memory);
         match inst {
             Inst::Imm(data) => regs.imm(data),
             Inst::Ins(digit) => regs.ins(digit),
@@ -50,14 +50,14 @@ impl State {
             Inst::Shr => regs.shr(),
             Inst::Rotl => regs.rotl(),
             Inst::Rotr => regs.rotr(),
-            Inst::Load => self.load(),
-            Inst::Store => self.store(),
-            Inst::Delete => self.delete(),
-            Inst::Put => self.put(),
-            Inst::Get => self.get(),
-            Inst::Save => self.save(),
-            Inst::Restore => self.restore(),
-            Inst::Quote(input) => self.quote(&input),
+            Inst::Load => page.load(),
+            Inst::Store => page.store(),
+            Inst::Delete => page.delete(),
+            Inst::Put => page.put(),
+            Inst::Get => page.get(),
+            Inst::Save => page.save(),
+            Inst::Restore => page.restore(),
+            Inst::Quote(input) => page.quote(&input),
             Inst::Call(_)
             | Inst::Define(_, _)
             | Inst::Macro(_, _)
@@ -79,57 +79,6 @@ impl State {
         }
         self.regs.acc = count;
     }
-    fn load(&mut self) {
-        self.regs.data = self.current()[0];
-    }
-    fn store(&mut self) {
-        self.current_mut()[0] = self.regs.data;
-    }
-    fn delete(&mut self) {
-        self.current_mut()[0] = 0;
-    }
-    fn put(&mut self) {
-        use io::Write;
-        let src = &self.current()[0..1];
-        let result = io::stdout().write(src);
-        self.regs.set_error(result.is_err());
-    }
-    fn get(&mut self) {
-        use io::Read;
-        let dst = &mut self.current_mut()[0..1];
-        let result = io::stdin().read(dst);
-        self.regs.set_error(result.is_err());
-    }
-    fn save(&mut self) {
-        let buf = &mut [0; 4];
-        self.regs.save(buf);
-        copy(self.current_mut(), buf);
-    }
-    fn restore(&mut self) {
-        let buf = &mut [0; 4];
-        copy(buf, self.current());
-        self.regs.restore(buf);
-    }
-    fn quote(&mut self, input: &str) {
-        copy(self.current_mut(), input.as_bytes());
-    }
-    fn page(&self) -> &[u8] {
-        self.memory[self.regs.block].get().iter().as_slice()
-    }
-    fn page_mut(&mut self) -> &mut [u8] {
-        self.memory[self.regs.block]
-            .get_mut()
-            .iter_mut()
-            .into_slice()
-    }
-    fn current(&self) -> &[u8] {
-        let coord = usize::from(self.regs.coord);
-        &self.page()[coord..]
-    }
-    fn current_mut(&mut self) -> &mut [u8] {
-        let coord = usize::from(self.regs.coord);
-        &mut self.page_mut()[coord..]
-    }
     pub fn print(&self, key: char) {
         self.regs.print(key);
         for y in 0..BLOCK_SIDE {
@@ -141,7 +90,7 @@ impl State {
     }
     fn print_cell(&self, x: u8, y: u8) {
         let index = x + y * BLOCK_SIDE;
-        let val = self.page()[usize::from(index)];
+        let val = self.memory[self.regs.block][index];
         Screen::print_display(util::as_hex(val), self.regs.coord == index);
     }
     fn move_cell(x: u8, y: u8) {
@@ -151,8 +100,4 @@ impl State {
         let y = u16::from(y) + LINE_OFFSET;
         Screen::move_cursor(x, y);
     }
-}
-
-fn copy(dst: &mut [u8], src: &[u8]) {
-    dst.iter_mut().zip(src).for_each(|(dst, src)| *dst = *src);
 }
