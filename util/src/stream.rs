a535113e96067aff167706e4053746c088ee3c60
hatsusato <2765630+hatsusato@users.noreply.github.com>
2023-03-29 00:28:00 +0900

    Add Kind::Stack

--- util/src/stream.rs
+++ util/src/stream.rs
@@ -1,7 +1,6 @@
 use std::collections::VecDeque;
 use std::io::{Read, Result, Write};
 use std::path::Path;
-use std::{fs, io};
 
 #[derive(Debug)]
 enum Kind {
@@ -9,8 +8,9 @@ enum Kind {
     Stdin,
     Stdout,
     Stderr,
-    File(fs::File),
+    File(std::fs::File),
     Queue(VecDeque<u8>),
+    Stack(Vec<u8>),
 }
 impl Default for Kind {
     fn default() -> Self {
@@ -52,7 +52,7 @@ impl Stream {
         let kind = Kind::Stderr;
         Self { kind }
     }
-    fn make_file(file: fs::File) -> Self {
+    fn make_file(file: std::fs::File) -> Self {
         let kind = Kind::File(file);
         Self { kind }
     }
@@ -60,27 +60,36 @@ impl Stream {
         let kind = Kind::Queue(VecDeque::new());
         Self { kind }
     }
+    pub fn make_stack() -> Self {
+        let kind = Kind::Stack(Vec::new());
+        Self { kind }
+    }
     /// # Errors
     pub fn open<P: AsRef<Path>>(path: P, flag: &Flag) -> Result<Self> {
-        let mut options = fs::File::options();
+        let mut options = std::fs::File::options();
         options.read(flag.is_read()).write(flag.is_write());
         options.open(path).map(Self::make_file)
     }
     pub fn get(&mut self) -> Option<u8> {
         match &mut self.kind {
-            Kind::Stdin => read(&mut io::stdin()),
+            Kind::Stdin => read(&mut std::io::stdin()),
             Kind::File(file) => read(file),
             Kind::Queue(queue) => queue.pop_front(),
+            Kind::Stack(stack) => stack.pop(),
             _ => None,
         }
     }
     pub fn put(&mut self, data: u8) -> Option<()> {
         match &mut self.kind {
-            Kind::Stdout => write(&mut io::stdout(), data),
-            Kind::Stderr => write(&mut io::stderr(), data),
+            Kind::Stdout => write(&mut std::io::stdout(), data),
+            Kind::Stderr => write(&mut std::io::stderr(), data),
             Kind::File(file) => write(file, data),
             Kind::Queue(queue) => {
                 queue.push_back(data);
+                Some(())
+            }
+            Kind::Stack(stack) => {
+                stack.push(data);
                 Some(())
             }
             _ => None,
