999c18fcaf1ce77e26c8b26d2a4816340fed6c58
hatsusato <2765630+hatsusato@users.noreply.github.com>
2023-03-28 20:38:58 +0900

    Move Args

---
+++ bed/src/args.rs
@@ -0,0 +1,50 @@
+use clap::Parser;
+use std::fs::File;
+use std::io::{BufReader, Read, Result};
+use std::path::PathBuf;
+use util::{Flag, Stream};
+
+#[derive(Debug, Parser)]
+#[command(version, about)]
+pub struct Args {
+    #[arg(help = "target source file\ninvoke interactive mode if omitted")]
+    source: Option<PathBuf>,
+    #[arg(short = 'i', long, help = "stdin source file")]
+    input: Option<PathBuf>,
+    #[arg(short = 'o', long, help = "stdout destination file")]
+    output: Option<PathBuf>,
+}
+impl Args {
+    pub fn is_interactive(&self) -> bool {
+        self.source.is_none()
+    }
+    pub fn open_code(&self) -> Result<Vec<u8>> {
+        Ok(match self.source.as_ref() {
+            Some(path) => {
+                let file = File::open(path)?;
+                let mut buf = String::new();
+                BufReader::new(file).read_to_string(&mut buf)?;
+                buf.into_bytes()
+            }
+            None => Vec::default(),
+        })
+    }
+    pub fn open_input(&self) -> Result<Stream> {
+        Ok(if let Some(path) = self.output.as_ref() {
+            Stream::open(path, &Flag::Read)?
+        } else if self.is_interactive() {
+            Stream::default()
+        } else {
+            Stream::stdin()
+        })
+    }
+    pub fn open_output(&self) -> Result<Stream> {
+        Ok(if let Some(path) = self.output.as_ref() {
+            Stream::open(path, &Flag::Write)?
+        } else if self.is_interactive() {
+            Stream::default()
+        } else {
+            Stream::stdout()
+        })
+    }
+}
--- bed/src/main.rs
+++ bed/src/main.rs
@@ -1,55 +1,10 @@
+mod args;
+
+use crate::args::Args;
 use clap::Parser;
-use std::fs::File;
-use std::io::{BufReader, Read, Result};
-use std::path::PathBuf;
-use util::{Flag, Stream};
+use std::io::Result;
 use view::Editor;
 use vm::Machine;
-
-#[derive(Debug, Parser)]
-#[command(version, about)]
-struct Args {
-    #[arg(help = "target source file\ninvoke interactive mode if omitted")]
-    source: Option<PathBuf>,
-    #[arg(short = 'i', long, help = "stdin source file")]
-    input: Option<PathBuf>,
-    #[arg(short = 'o', long, help = "stdout destination file")]
-    output: Option<PathBuf>,
-}
-impl Args {
-    fn is_interactive(&self) -> bool {
-        self.source.is_none()
-    }
-    fn open_code(&self) -> Result<Vec<u8>> {
-        Ok(match self.source.as_ref() {
-            Some(path) => {
-                let file = File::open(path)?;
-                let mut buf = String::new();
-                BufReader::new(file).read_to_string(&mut buf)?;
-                buf.into_bytes()
-            }
-            None => Vec::default(),
-        })
-    }
-    fn open_input(&self) -> Result<Stream> {
-        Ok(if let Some(path) = self.output.as_ref() {
-            Stream::open(path, &Flag::Read)?
-        } else if self.is_interactive() {
-            Stream::default()
-        } else {
-            Stream::stdin()
-        })
-    }
-    fn open_output(&self) -> Result<Stream> {
-        Ok(if let Some(path) = self.output.as_ref() {
-            Stream::open(path, &Flag::Write)?
-        } else if self.is_interactive() {
-            Stream::default()
-        } else {
-            Stream::stdout()
-        })
-    }
-}
 
 fn main() {
     let args = Args::parse();
